7d7c41efab6dfbf88f72cb2e5797acd5b2df90ec
Brigitte Bernal Belisario <131219987+Fx2048@users.noreply.github.com>
2024-06-24 17:39:10 -0500

    Update deploy_security.py

--- src/utils/deploy_security.py
+++ src/utils/deploy_security.py
@@ -1,1 +1,10 @@
+import boto3
+from botocore.exceptions import ClientError
+
+def deploy_security():
+    # Código para desplegar y configurar la seguridad en AWS
+    pass
+
+if __name__ == "__main__":
+    deploy_security()
 
